e212a207aa039e21fcac632bce3992e01b60cb29
Gus Doriguetto <doriguetto@adobe.com>
2024-10-16 10:43:17 +1100

    svelte sample

---
+++ src/blocks/sample/Sample.svelte
@@ -0,0 +1,28 @@
+<script>
+    import Time from './Time.svelte';
+    import {onMount} from "svelte";
+
+    let timeComponent;
+    let time;
+
+    onMount(() => {
+        debugger;
+    })
+
+</script>
+
+<div>
+    <div>
+        <Time bind:this={timeComponent} bind:time />
+        <h1>Spent time: {time} ms</h1>
+
+        {#if timeComponent}
+            <button on:click={() => timeComponent.spendTime() }>Click me</button>
+        {:else}
+            Loading...
+        {/if}
+    </div>
+</div>
+
+
+
