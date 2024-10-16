400ce019582c726e094dab8667c3b9cc5963e9b0
Gus Doriguetto <doriguetto@adobe.com>
2024-10-16 16:51:19 +1100

    update svelte component

--- src/blocks/sample/sample.ts
+++ src/blocks/sample/sample.ts
@@ -1,24 +1,26 @@
 import Sample from './Sample.svelte';
 
 export default async function decorate(block) {
-    debugger;
-
-    // Select the node that will be observed for mutations
-    var targetNode = block;
 
     // Options for the observer (which mutations to observe)
-    var config = { attributes: true, childList: true };
+    var config = { attributes: true, childList: true, subtree: true };
 
     // Callback function to execute when mutations are observed
     var callback = function(mutationsList) {
         debugger
         for(var mutation of mutationsList) {
             if (mutation.type == 'childList') {
-                console.log('A child node has been added or removed.');
-                console.log(mutation.target)
+                mutation.addedNodes.forEach(node => {
+                    console.log('ADDED')
+                    console.log(node);
+                })
+                mutation.removedNodes.forEach(node => {
+                    console.log('REMOVED')
+                    console.log(node);
+                })
             }
             else if (mutation.type == 'attributes') {
-                console.log('The ' + mutation.attributeName + ' attribute was modified.');
+                console.log('ATTR:  ' + mutation.attributeName);
             }
         }
     };
@@ -27,8 +29,9 @@ export default async function decorate(block) {
     var observer = new MutationObserver(callback);
 
     // Start observing the target node for configured mutations
-    observer.observe(targetNode, config);
+    observer.observe(block, config);
 
+    console.log(block.outerHTML);
 
     new Sample({
         target: block,
