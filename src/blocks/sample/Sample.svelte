8a748e6434e7805a476180123ddad86e1cc5549a
Gus Doriguetto <doriguetto@adobe.com>
2024-10-16 16:53:18 +1100

    update svelte component

--- src/blocks/sample/Sample.svelte
+++ src/blocks/sample/Sample.svelte
@@ -1,27 +1,5 @@
-<script>
-    import Time from './Time.svelte';
-    import {onMount} from "svelte";
-
-    let timeComponent;
-    let time;
-
-    onMount(() => {
-        debugger;
-    })
-
-</script>
-
 <div>
-    <div>
-        <Time bind:this={timeComponent} bind:time />
-        <h1>Spent time: {time} ms</h1>
-
-        {#if timeComponent}
-            <button on:click={() => timeComponent.spendTime() }>Click me</button>
-        {:else}
-            Loading...
-        {/if}
-    </div>
+    <div><p>One day you will make It.</p></div>
 </div>
 
 
