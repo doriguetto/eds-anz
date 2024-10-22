5c09eedc07986e4d643ba86a8a061883a8133640
Gus Doriguetto <doriguetto@adobe.com>
2024-10-22 14:15:44 +1100

    update header

--- src/blocks/hero/image.tsx
+++ src/blocks/hero/image.tsx
@@ -19,10 +19,13 @@ const ImagePreloader = ({ breakpoints = [], eager = false, pictureEl }: ImagePre
 
     if (!imgElSrc) return null;
     const alt = img?.getAttribute('alt') || '';
+    const width = img?.getAttribute('width') || 0;
+    const height = img?.getAttribute('height') || 0;
 
     const url = new URL(imgElSrc, window.location.href);
     const { pathname } = url;
     const ext = pathname.substring(pathname.lastIndexOf('.') + 1);
+    const imgSrc = `${pathname}?width=2048&amp;format=jpeg&amp;optimize=medium`
 
     const sources = breakpoints.map((breakpoint) => {
         return {
@@ -38,21 +41,26 @@ const ImagePreloader = ({ breakpoints = [], eager = false, pictureEl }: ImagePre
     }, []);
 
     return (
-        <picture>
-            {sources.map((source, index) => (
-                <>
-                    <source key={index} media={source.media} srcSet={source.srcSet}/>
-                    <source key={index} media={source.media} type={source.type} srcSet={source.srcSet}/>
-                    {index === sources.length - 1 && (
-                        <img
-                            loading={eager? 'eager' :'lazy'}
-                            alt={alt}
-                            src={`${pathname}?width=2048&amp;format=jpeg&amp;optimize=medium`}
-                        />
-                    )}
-                </>
-            ))}
-        </picture>
+        <>
+            <link rel="preload" as="image" href={imgSrc}></link>
+            <picture>
+                {sources.map((source, index) => (
+                    <>
+                        <source key={index} media={source.media} srcSet={source.srcSet}/>
+                        <source key={index} media={source.media} type={source.type} srcSet={source.srcSet}/>
+                        {index === sources.length - 1 && (
+                            <img
+                                loading={eager? 'eager' :'lazy'}
+                                alt={alt}
+                                width={width}
+                                height={height}
+                                src={`${pathname}?width=2048&amp;format=jpeg&amp;optimize=medium`}
+                            />
+                        )}
+                    </>
+                ))}
+            </picture>
+        </>
     );
 };
 
